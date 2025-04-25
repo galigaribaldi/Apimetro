ef2a185d741d6cc47e5df3ffc1fd67e188f6da72
Galileo garibaldi <galigaribaldi@live.com>
2025-04-25 13:21:09 -0600

    Merge with DEV

--- cmd/pkg/controller/Controller.go
+++ cmd/pkg/controller/Controller.go
@@ -12,14 +12,9 @@ import (
 var DB *gorm.DB
 
 func ConnectDataBase() {
-<<<<<<< HEAD:cmd/pkg/controller/Controller.go
-	log.Println("Variable: ",os.Getenv("DATABASE_URL"))
-	dbURL := "postgresql://postgres:postgress@localhost:5432/data";
-=======
 	log.Println(os.Getenv("DATABASE_URL"))
 	//dbURL := "postgresql://postgres:postgress@localhost:5433/Data"
 	dbURL := os.Getenv("DATABASE_URL")
->>>>>>> main:pkg/controller/Controller.go
 	//postgresql://postgres:postgress@localhost:5433/Data
 
 	database, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
